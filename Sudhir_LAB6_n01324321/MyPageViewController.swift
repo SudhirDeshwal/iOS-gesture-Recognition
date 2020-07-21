39f2e77bfa6909634d1e657394b192e38d9d757a
Sudhir Deshwal <DES>
2020-07-21 00:47:24 -0400

    PAGE View controller is working

--- Sudhir_LAB6_n01324321/MyPageViewController.swift
+++ Sudhir_LAB6_n01324321/MyPageViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class MyPageViewController: UIPageViewController {
+class MyPageViewController: UIPageViewController, UIPageViewControllerDataSource ,UIPageViewControllerDelegate {
+    
+    
+    
+    lazy var subViewControllers:[UIViewController] =  {
+        
+        return [
+        
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController,
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController,
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ThirdViewController") as! ThirdViewController
+        ]
+        
+    } ()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
+        self.delegate = self
+        self.dataSource = self
+        
+        setViewControllers([subViewControllers[0]], direction: .forward, animated: true, completion: nil)
         
     }
     
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return subViewControllers.count
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let currentIndex:Int = subViewControllers.index(of : viewController) ?? 0
+        
+        
+        if(currentIndex <= 0) {
+            
+            return nil
+        }
+        
+        return subViewControllers[currentIndex-1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+         let currentIndex:Int = subViewControllers.index(of : viewController) ?? 0
+        
+        if(currentIndex >= subViewControllers.count-1) {
+            
+            return nil
+        }
+        
+        return subViewControllers[currentIndex+1]
+        
+    }
 
 }
+
+
